9c263760321e8b661844588fb745cc1b518817c2
Tyler Toomey <toomey@rowan.edu>
2022-07-07 23:01:47 -0400

    Changes

--- components/Preview.svelte
+++ components/Preview.svelte
@@ -5,7 +5,7 @@
     import { onDestroy } from 'svelte'
 
     
-    const DEFAULT = "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg";
+    const DEFAULT = ""//https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg";
    
     let uploadButton, previewImage;
 
